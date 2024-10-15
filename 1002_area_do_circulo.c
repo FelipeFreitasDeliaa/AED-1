27110ca1d6829ba4f1162b95e190d83526e68aa5
FelipeFreitasDeliaa <ffdelia@unifesp.br>
2024-10-15 14:02:46 -0300

    Create 1002_area_do_circulo.c

---
+++ 1002_area_do_circulo.c
@@ -0,0 +1,19 @@
+/*Felipe Freitas D' Elia
+            Ra: 176284*/
+
+#include <stdio.h>´
+#include <math.h>
+ 
+int main() {
+ 
+    double raio;
+    double area;
+    
+    scanf ("%lf", &raio);
+    
+    area = 3.14159 * (pow (raio, 2));
+    
+    printf ("A=%.4lf\n",area);
+    
+    return 0;
+}
